34fbc02a395c7a151f88540d6e3b34cd885e627e
Abdellah Sabiri <abdellah.sabiri.1995@gmail.com>
2019-07-07 23:08:04 +0100

    first commit

---
+++ caroussel.js
@@ -0,0 +1,84 @@
+function caroussel(config, data){
+    this.CLASS = config.CLASS || 'caroussel';
+    this.MAX_ELEMENT = config.MAX_ELEMENT || 6;
+    this.DELAY_TIME = config.DELAY_TIME || 500;
+    this.currentMax = this.MAX_ELEMENT;
+    this.dataLength = data.length;
+    this.data = data;
+
+    const carousselDiv = document.querySelector("."+this.CLASS);
+
+    carousselDiv.innerHTML += `<div class="caroussel-prev delete-button">&lt;</div><div class='caroussel-elements row'>`;
+    for (let i=0; i<MAX_ELEMENT; i++){
+        let div = "<div class='col-2 caroussel-element'>";
+        div += `<a href="#"><img src='${data[i].image}' class="w-100"/></a>`;
+        div += "</div>";
+        document.querySelector(".caroussel-elements").innerHTML += div;
+    }
+    carousselDiv.innerHTML += `</div><div class="caroussel-next appear-button">&gt;</div>`;
+
+    const prevButton = document.querySelector(".caroussel-prev");
+    const nextButton = document.querySelector(".caroussel-next");
+
+    nextButton.addEventListener("click", (event)=>{
+        if(this.currentMax >= this.dataLength){
+            return;
+        }
+        this.currentMax++;
+        const elements = document.querySelectorAll(".caroussel-element");
+        elements[0].classList.add('delete');
+        elements.forEach((elm)=>{
+            elm.classList.add("translate-left");
+        });
+        setTimeout(()=>{
+            elements[0].remove();
+            elements.forEach((elm)=>{
+                elm.classList.remove("translate-left");
+            });
+            let div = "<div class='col-2 caroussel-element'>";
+            div += `<a href="#"><img src='${data[this.currentMax-1].image}' class="w-100"/></a>`;
+            div += "</div>";
+            document.querySelector(".caroussel-elements").innerHTML += div;
+        },DELAY_TIME);
+        this.checkButtons();
+    });
+
+    prevButton.addEventListener("click", (event)=>{
+        if(this.currentMax <= this.MAX_ELEMENT){return;}
+        this.currentMax--;
+        const elements = document.querySelectorAll(".caroussel-element");
+        elements[MAX_ELEMENT-1].classList.add('delete');
+        elements.forEach((elm)=>{
+            elm.classList.add("translate-right");
+        });
+        setTimeout(()=>{
+            elements[MAX_ELEMENT-1].remove();
+            elements.forEach((elm)=>{
+                elm.classList.remove("translate-right");
+            });
+            let div = "<div class='col-2 caroussel-element'>";
+            div += `<a href="#"><img src='${data[currentMax -MAX_ELEMENT].image}' class="w-100"/></a>`;
+            div += "</div>";
+            document.querySelector(".caroussel-elements").innerHTML = div + document.querySelector(".caroussel-elements").innerHTML ;
+        },DELAY_TIME);
+        this.checkButtons();
+    });
+
+    this.checkButtons = function(){
+        if(this.currentMax === this.dataLength){
+            nextButton.classList.add("delete-button");
+            nextButton.classList.remove("appear-button");
+        }else{
+            nextButton.classList.remove("delete-button");
+            nextButton.classList.add("appear-button");
+        }
+        if(this.currentMax <= this.MAX_ELEMENT){
+            prevButton.classList.add("delete-button");
+            prevButton.classList.remove("appear-button");
+        }else{
+            prevButton.classList.remove("delete-button");
+            prevButton.classList.add("appear-button");
+        }
+    }
+
+}
---
+++ mock/data.js
@@ -0,0 +1,44 @@
+mockData = [
+  {
+    "_id": "5d2249069e694bab44ab68b6",
+    "image": "https://picsum.photos/128/128?random=1",
+    "text": "Jaclyn",
+    "link": "#"
+  },
+  {
+    "_id": "5d224906ae164ffb20295ce4",
+    "image": "https://picsum.photos/128/128?random=2",
+    "text": "Julia",
+    "link": "#"
+  },
+  {
+    "_id": "5d224906c4942d2738f72333",
+    "image": "https://picsum.photos/128/128?random=3",
+    "text": "Rich",
+    "link": "#"
+  },
+  {
+    "_id": "5d2249065494b9727214278f",
+    "image": "https://picsum.photos/128/128?random=4",
+    "text": "Mckenzie",
+    "link": "#"
+  },
+  {
+    "_id": "5d22490607058e0d3c2c7cf1",
+    "image": "https://picsum.photos/128/128?random=5",
+    "text": "Verna",
+    "link": "#"
+  },
+  {
+    "_id": "5d224906a9d194eb90fcfadd",
+    "image": "https://picsum.photos/128/128?random=6",
+    "text": "Merritt",
+    "link": "#"
+  },
+  {
+    "_id": "5d224906df83191401cb545e",
+    "image": "https://picsum.photos/128/128?random=7",
+    "text": "Kendra",
+    "link": "#"
+  }
+]
